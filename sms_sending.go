37620672071798b4f9bd8948bfbaf7ac7e79b4fa
Bodhi14 <bodhi140802@gmail.com>
2022-08-29 02:15:53 +0530

    SMS Sending

---
+++ sms_sending.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	textmagic "github.com/textmagic/textmagic-rest-go"
+)
+
+func main() {
+	client := textmagic.NewClient("<USERNAME>", "<APIV2_KEY>")
+	params := map[string]string{
+		"phones": "+919073423666",
+		"text":   "Hello from TextMagic Go",
+	}
+	message, err := client.CreateMessage(params)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(message)
+	}
+}
