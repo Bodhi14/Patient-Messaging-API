55e47f018a57715b30d61d2c3e219dfa354de8d2
Bodhi14 <bodhi140802@gmail.com>
2022-08-29 02:46:56 +0530

    SMS Sending

--- sms_sending.go
+++ sms_sending.go
@@ -1,21 +1,132 @@
 package main
 
 import (
-	"fmt"
-
-	textmagic "github.com/textmagic/textmagic-rest-go"
+	textlocal "../../textlocal.app.mjs"
 )
 
 func main() {
-	client := textmagic.NewClient("<USERNAME>", "<APIV2_KEY>")
-	params := map[string]string{
-		"phones": "+919073423666",
-		"text":   "Hello from TextMagic Go",
-	}
-	message, err := client.CreateMessage(params)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(message)
-	}
+  key := "textlocal-send-sms"
+  name := "Send SMS"
+  description :=
+    `This Action can be used to send text messages to either individual numbers or entire contact groups. [See the docs here](https://api.txtlocal.com/docs/sendsms)
+    Note: While both numbers and group_id are optional parameters, one or the other must be included in the request for the message to be sent.`
+  version := "0.0.1"
+  var types string
+  types = "action"
+  props := {
+    textlocal,
+    sender: {
+      propDefinition: [
+        textlocal,
+        "Bodhisattwa",
+      ],
+    },
+    message: {
+      propDefinition: [
+        textlocal,
+        "Hello bro!!! This is my first message sent using SMS API of TextLocal",
+      ],
+    },
+    numbers: {
+      propDefinition: [
+        textlocal,
+        "+919073423666" + "+919830729594",
+      ],
+      description: "Note: While both numbers and group_id are optional parameters, one or the other must be included in the request for the message to be sent.",
+      optional: true,
+    },
+    groupId: {
+      propDefinition: [
+        textlocal,
+        "",
+      ],
+      description: "Note: While both numbers and group_id are optional parameters, one or the other must be included in the request for the message to be sent.",
+      optional: true,
+    },
+    simpleReply: {
+      propDefinition: [
+        textlocal,
+        "simpleReply",
+      ],
+      optional: true,
+    },
+    scheduleTime: {
+      propDefinition: [
+        textlocal,
+        "scheduleTime",
+      ],
+      optional: true,
+    },
+    receiptUrl: {
+      propDefinition: [
+        textlocal,
+        "receiptUrl",
+      ],
+      optional: true,
+    },
+    custom: {
+      propDefinition: [
+        textlocal,
+        "custom",
+      ],
+      optional: true,
+    },
+    optouts: {
+      propDefinition: [
+        textlocal,
+        "optouts",
+      ],
+      optional: true,
+    },
+    validity: {
+      propDefinition: [
+        textlocal,
+        "validity",
+      ],
+      optional: true,
+    },
+    unicode: {
+      propDefinition: [
+        textlocal,
+        "unicode",
+      ],
+      optional: true,
+    },
+    trackingLinks: {
+      propDefinition: [
+        textlocal,
+        "trackingLinks",
+      ],
+      optional: true,
+    },
+    test: {
+      propDefinition: [
+        textlocal,
+        "test",
+      ],
+    },
+  },
+  async run({ $ }) {
+    const params = {
+      numbers: this.numbers,
+      sender: this.sender,
+      message: this.message,
+      groupId: this.groupId,
+      simple_reply: this.simpleReply,
+      schedule_time: this.scheduleTime,
+      receipt_url: this.receiptUrl,
+      custom: this.custom,
+      optouts: this.optouts,
+      validity: this.validity,
+      unicode: this.unicode,
+      tracking_links: this.trackingLinks,
+      test: this.test,
+    };
+    const response = await this.textlocal.sendSMS({
+      $,
+      params,
+    });
+    $.export("$summary", "Successfully sent SMS");
+    return response;
+  },
 }
